837e018695fed7472fb9a579879d56056b72b6a0
Kunal Patil <kunalpatil2203@gmail.com>
2025-07-04 21:54:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,63 @@
+// Fetch todos from local storage
+let todos = localStorage.getItem("todos");
+
+// Initialize todos as an empty array if it doesn't already exist in local storage
+if (todos == null) {
+  todos = [];
+}
+
+// Event to handle request to render todo list on page load
+document.addEventListener("DOMContentLoaded", (e) => renderTodoList());
+
+// Event to handle request for adding new todo
+document.getElementById("todo-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formdata = new FormData(e.target);
+
+  const title = formdata.get("title");
+
+  const todo = { id: crypto.randomUUID(), title, is_done: false };
+
+  todos.push(todo);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  renderTodoList();
+
+  e.target.reset();
+});
+
+function renderTodoList() {
+  const todo_list_el = document.getElementById("todo-list");
+  let todo_list = "";
+  todos.forEach((todo) => {
+    todo_list += `
+            <li >
+                <div>
+                    <input type="checkbox" ${
+                      todo.is_done ? "checked" : ""
+                    } onchange="updateTodo(event);" data-todo-id="${todo.id}" />
+                    <p>${todo.title}</p>
+                </div>
+                <button onclick="deleteTodo(event);" data-todo-id="${
+                  todo.id
+                }">&times;</button>
+            </li>
+        `;
+  });
+
+  todo_list_el.innerHTML = todo_list;
+}
+
+function updateTodo(e) {
+  const index = todos.findIndex((todo) => todo.id == e.target.dataset.todoId);
+  todos[index].is_done = e.target.checked;
+  localStorage.setItem("todos", JSON.stringify(todos));
+  renderTodoList();
+}
+
+function deleteTodo(e) {
+  todos = todos.filter((todo) => todo.id != e.target.dataset.todoId);
+  localStorage.setItem("todos", JSON.stringify(todos));
+  renderTodoList();
+}
